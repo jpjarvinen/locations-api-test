0ad486b78427e862e4a2728ae231c36aca293ed7
Jukka-Pekka Järvinen <jukka.pekka.j.jarvinen@gmail.com>
2021-03-03 12:41:11 +0200

    Pretty print

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ let customers = [
   { id: 1, name: "Jack" },
   { id: 2, name: "Tina" },
 ];
+
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+var str = JSON.stringify(database, null, 2)
+
 // HTTP GET http://localhost:8080/api/customers
 app.get("/api/customers", (req, res) => {
   res.send(customers);
 });
+
+// HTTP GET http://localhost:8080/api/locations
+app.get('/api/locations', function (req, res) {
+  //res.json('fetch all locations')
+  //res.json(database)
+  res.type("application/json")
+  res.send(str)
+  //res.send(JSON.stringify(database, null, 2))
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
